24658507c01e711924fab0e24ee0d1f600e4df8d
JoshisonGitHub <joshua.d.pulham@durham.ac.uk>
2022-12-29 22:35:29 +0100

    Add 18 working buttons with image carousels. Add backend code (work in progress). Add client sided js file. Add express

---
+++ Front End/clientside.js
@@ -0,0 +1,101 @@
+var x = document.getElementById("l1content");
+function change(){
+    //code sourced from https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+function l1() {
+  x = document.getElementById("l1content");
+  change();
+}
+
+function l2(){
+    x = document.getElementById("l2content");
+    change();
+}
+
+function l3(){
+    x = document.getElementById("l3content");
+    change();
+}
+
+function l4(){
+    x = document.getElementById("l4content");
+    change();
+}
+
+function l5(){
+    x = document.getElementById("l5content");
+    change();
+}
+
+function l6(){
+    x = document.getElementById("l6content");
+    change();
+}
+
+function l7(){
+    x = document.getElementById("l7content");
+    change();
+}
+
+function l8(){
+    x = document.getElementById("l8content");
+    change();
+}
+
+function l9(){
+    x = document.getElementById("l9content");
+    change();
+}
+
+function l10(){
+    x = document.getElementById("l10content");
+    change();
+}
+
+function l11(){
+    x = document.getElementById("l11content");
+    change();
+}
+
+function l12(){
+    x = document.getElementById("l12content");
+    change();
+}
+
+function l13(){
+    x = document.getElementById("l13content");
+    change();
+}
+
+function l14(){
+    x = document.getElementById("l14content");
+    change();
+}
+
+function l15(){
+    x = document.getElementById("l15content");
+    change();
+}
+
+function l16(){
+    x = document.getElementById("l16content");
+    change();
+}
+
+function l17(){
+    x = document.getElementById("l17content");
+    change();
+}
+
+function l18(){
+    x = document.getElementById("l18content");
+    change();
+}
+
