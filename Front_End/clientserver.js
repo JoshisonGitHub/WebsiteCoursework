c3f044524001249ebf93e36ca625d179f992daf0
JoshisonGitHub <joshua.d.pulham@durham.ac.uk>
2023-01-14 15:44:01 +0000

    Change Picture to base64, can not store it yet though. Add get method for gamenames. Add functioning Post method to json database

--- Front_End/clientserver.js
+++ Front_End/clientserver.js
@@ -5,19 +5,22 @@ const endpointRoot = "http://127.0.0.1:8090/";
 let form = document.querySelector('#game-submit');
 let file = document.querySelector('#file');
 let appnew = document.querySelector('#newoutput');
-
-
+let img = document.createElement('div');
+let textvar = "";
 /**
  * Log the uploaded file to the console
  * @param {event} Event The file loaded event
  */
+
 function logFile (event) {
   let str = event.target.result;
-  let img = document.createElement('img');
+  
   
   img.src = str;
-  console.log(img.src);
+  //console.log(img.src);
   appnew.append(img);
+  //addgames();
+  //console.log(appnew);
   
 }
 
@@ -25,51 +28,115 @@ function logFile (event) {
  * Handle submit events
  * @param  {Event} event The event object
  */
+
 function handleSubmit (event) {
 
   // Stop the form from reloading the page
   event.preventDefault();
 
   // If there's no file, do nothing
-  if (!file.value.length) return;
+  if (!file.value.length){
+    return;
+  } 
 
   // Create a new FileReader() object
-  let reader = new FileReader();
+  //let reader = new FileReader();
 
   // Setup the callback event to run when the file is read
-  reader.onload = logFile;
-
+  //reader.onload = logFile;
+  const imgnew = readFile(file.files[0]);
+ 
+ 
+  imgnew.then((val) => img.append(val));
+  //console.log(img);
+  //appnew.append(img);
+  
   // Read the file
-  reader.readAsDataURL(file.files[0]);
+  //reader.readAsDataURL(file.files[0]);
+  
 
 }
+
+
+
+
 
 // Listen for submit events
 form.addEventListener('submit', handleSubmit);
 
+function readFile(file){
+  return new Promise((resolve, reject) => {
+    var fr = new FileReader();  
+    fr.onload = () => {
+      resolve(fr.result )
+    };
+    fr.onerror = reject;
+    fr.readAsDataURL(file);
+  });
+}
 
 async function addgames() {
+  
+    
+
+  
+
   const gameForm = document.getElementById('game-submit');
-  console.log("it worked");
+  //console.log("it worked");
   gameForm.addEventListener("submit", async function (event) {
-      event.preventDefault();
-      // eslint-disable-next-line no-undef
-      const data = new FormData(gameForm);
-  /* conversion from FormData to JSON at https://stackoverflow.com/questions/41431322/how-to-convert-formdata-html5-object-to-json */
-      const dataJSON = JSON.stringify(Object.fromEntries(data));
-      // eslint-disable-next-line no-unused-vars
-      const response = await fetch(endpointRoot + 'game/new',
-      {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-            },
-          body: dataJSON
-      });
+    event.preventDefault();
+
+    
+    // eslint-disable-next-line no-undef
+    const data = new FormData(gameForm);
+    
+    
+    
+    //data.picture = base64 pic
+
+    /* conversion from FormData to JSON at https://stackoverflow.com/questions/41431322/how-to-convert-formdata-html5-object-to-json */
+    const dataJSON = JSON.stringify(Object.fromEntries(data));
+
+    //console.log(dataJSON);
+    
+    console.log(dataJSON);
+    
+    
+
+  
+    /*
+    // Create a new FileReader object
+    const reader = new FileReader();
+
+    // Add an event listener for when the file is fully loaded
+    reader.addEventListener('load', () => { 
+      // The result of the file reader is a data URL
+      // which can be used to create an image
+      const base64Image = reader.result;
+ 
+      list.picture = base64Image;
+    });
+    
+    // Read the file as a data URL
+    // this breaks it
+    reader.readAsDataURL(list.picture);
+    */
+    
+    
+
+    // eslint-disable-next-line no-unused-vars
+    const response = await fetch(endpointRoot + 'game/new',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: dataJSON
+    });
       
-      gameForm.reset();
+    gameForm.reset();
   });
-  }
+}
 
 document.addEventListener("DOMContentLoaded", addgames);
 
--- app.js
+++ app.js
@@ -18,15 +18,52 @@ app.get("/", (req, res)=>{
 const fs = require("fs");
 const gdb = "./gamedatabase.json";
 const games = require(gdb);
-
+//const game = {};
 
 app.post("/game/new", function (req, resp) {
-    const key = req.body.gamename;
-    const description = req.body.description;
+    const gamename = req.body.gamename;
+    const date = req.body.date;
     const picture = req.body.picture;
-    console.log(picture);
-    games[key] = description;
-    games[picture] = picture;
+
+    
+    //console.log(picture);
+    
+
+    
+       
+    
+    
+    /*
+    let gameExist = false;
+    Object.keys(games).forEach(function(key) {
+      if(games[key].gamename === gamename){
+        games["game"][gamename].push({date, picture});
+        gameExist = true;
+      }
+    });
+    if(!gameExist){
+        games["game"] = {[gamename]: [{date, picture}]};
+    }
+    else {
+        
+    }
+    
+    */
+   
+    if(!games["game"]){
+        games["game"] = {};
+    }
+
+    if(games["game"][gamename]){
+        games["game"][gamename].push({date, picture});
+    }
+    else {
+        games["game"][gamename] = [{date, picture}];
+    }
+
+
+
+
     fs.writeFileSync(gdb, JSON.stringify(games));
     resp.send(games);
 });
@@ -35,11 +72,71 @@ app.post("/game/new", function (req, resp) {
 
 
 
-/*
+
+
+
 app.get('/games', function (req, resp) {
-    const gamename = Object.keys(games);
-    resp.send(gamename);
+    const newname = Object.keys(games["game"]);
+    resp.send(newname);
 });
+fetch("http://localhost:8090/games")
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (text) {
+    console.log(text);
+  });
+
+
+  app.get("/game/:gamedata", function (req, resp) {
+    const gamedata = req.params.gamename;
+    if(games["game"][gamedata]) {
+        resp.json(games["game"][gamedata]);
+    }
+    else {
+        resp.status(404).send("Game not found");
+    }
+});
+fetch("http://localhost:8090/game/:gamedata")
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (text) {
+    console.log(text);
+  });
+
+
+/*
+let testdatabase = 
+{
+
+    "games": 
+    {
+        "gamename": 
+        [
+            {
+                date: "2003-01-20",
+                picture: "base64pic"
+            },
+            {
+                date: "200-04-10",
+                picture: "base64pic2"
+            }
+        ],
+        "different gamename":
+        [
+            {
+                date: "2013-11-22",
+                picture: "base64pic"
+            },
+            {
+                date: "200-04-10",
+                picture: "base64pic2"
+            }
+        ]
+        
+    }
+}
 */
 
 
@@ -49,3 +146,4 @@ app.listen(port, () => {
 
 
 module.exports = app;
+
